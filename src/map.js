f004f588f2c7555e76714249c9203e43f3f02e63
matiascarreras <mcarreras@descuentocity.com>
2022-05-19 17:26:07 -0300

    cambio google maps por leaflet

--- src/map.js
+++ src/map.js
@@ -4,13 +4,20 @@ export default {
   template: `<div class='vxmap'></div>`,
   
   mounted() {
+    const leaflet = require('leaflet');
+
     const options = {
       zoom: parseInt(this.zoom),
-      center: new google.maps.LatLng(this.lat, this.lng),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+      minZoom: 10,
+      maxZoom: 18,
+      zoomControl: true,
+      center: { lat: this.lat, lng: this.lng },
+    }
     
-    const map = new google.maps.Map(this.$el, options);
+    const map = leaflet.map(this.$el, options);
+    leaflet.tileLayer('https://{s}-tile.avuxicdn.com/avuxi-th2/{z}/{x}/{y}.png?tpkey=622bb407a904e838e65ebfd8', {
+      attribution: '<a href="https://www.avuxi.com" target="_blank">AVUXI</a>',
+    }).addTo(map);
     AVUXI.MapStart(map, this.confId);
   },
 }
